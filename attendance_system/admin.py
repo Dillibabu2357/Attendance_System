d27d70191ff437bd79b36344f37ff2ef44f32dc5
Dillibabu2357 <165015300+Dillibabu2357@users.noreply.github.com>
2024-04-03 12:18:53 +0530

    Update admin.py

--- attendance_system/admin.py
+++ attendance_system/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Attendance,Teacher,Student,Marks,Subject,Teacher_has_subject
+from .models import Attendance,Teacher,Student,Marks,Subject,Teacher_has_Subject
 
 admin.site.register(Attendance)
 admin.site.register(Teacher)
